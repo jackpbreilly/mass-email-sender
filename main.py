75eb7a0b84125d77a9ee61626d3d2e36b63e9c47
jackpbreilly <jackpbreilly@gmail.com>
2020-02-20 21:09:48 +0000

    multithreadeding reading in and printing

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
+from multiprocessing import Pool
 import time
 import csv
+import datetime
+
+def callProcessRow(row):
+    processRow(row)
+
+def processRow(row):
+    email = str(row)
+    print(email+"\n")
+
+class processCSV():
+
+    def __init__(self, fileName):
+        self.fileName = fileName
+
+    def getRowCount(self):
+        with open(self.fileName) as f:
+            for i, l in enumerate(f):
+                pass
+        self.rowCount = i
+
+    def selectChunkSize(self):
+        if(self.rowCount>10000):
+            self.chunkSize = 100
+            return
+        if(self.rowCount>5000):
+            self.chunkSize = 500
+            return
+        self.chunkSize = 10
+        return
+
+    def processRows(self):
+        listRows = []
+        count = 0
+        with open(self.fileName,mode='r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                listRows.append(row)
+                if(len(listRows) == self.chunkSize):
+                    p.map(callProcessRow, listRows)
+                    del listRows[:]
+
+    def startProcess(self):
+        self.getRowCount()
+        self.selectChunkSize()
+        self.processRows()
+
 
 start = time.time()
 
-### Printing out 1 row in CSV file
-with open('emailList.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-            print(f'\t{row[0]}')
+p = Pool(4)
+ob = processCSV("emailList.csv")
+ob.startProcess()
 
 end = time.time()
 
 print("Time taken to execute " + str(end - start) + "s")
+
+
+
 
 
