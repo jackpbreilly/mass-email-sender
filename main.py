8470e1dce8208dbe9dd4e3d60d7e616a3e2470fa
jackpbreilly <jackpbreilly@gmail.com>
2020-02-20 20:34:33 +0000

    adding in timer to time execution

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import time
 import csv
 
+start = time.time()
+
+### Printing out 1 row in CSV file
 with open('emailList.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
             print(f'\t{row[0]}')
+
+end = time.time()
+
+print("Time taken to execute " + str(end - start) + "s")
+
+
