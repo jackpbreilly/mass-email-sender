fe338f790460062f4370e4a78a1b0e76e4442e23
jackpbreilly <jackpbreilly@gmail.com>
2020-02-26 18:08:39 +0000

    no preformance increase changing method

--- main.py
+++
@@ -1,6 +0,0 @@
-import time
-import csv
-input_file = "emailList.csv"
-start_time = time.time()
-data = csv.DictReader(open(input_file))
-print("csv.DictReader took %s seconds" % (time.time() - start_time))
